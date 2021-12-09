130c22091ef34277b6b71f083cb4e911eb76d19e
Dmitriy <trishkindima098@gmail.com>
2021-12-09 20:51:43 +0300

    Создание класса Product

---
+++ Project/Project/Main.cpp
@@ -0,0 +1,12 @@
+#include "Product.h"
+
+
+int main()
+{
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	return 0;
+
+}
--- Project/Project/Product.h
+++ Project/Project/Product.h
@@ -1,1 +1,17 @@
-#pragma once
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <iomanip>
+#include <Windows.h>
+
+using namespace std;
+
+
+void getaLine(string& inStr); // получение строки текста
+char getaChar();
+
+class Product
+{
+
+};
